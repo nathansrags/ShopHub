4897250f1f0bf41d5b9842659ddd42390e7cd779
Gopi <nathansrags@gmail.com>
2024-08-05 22:02:09 +0530

    folder rename

--- UserManager/src/main/java/com/shophub/model/constants/ApiConstants.java
+++
@@ -1,6 +0,0 @@
-package com.shophub.model.constants;
-
-public class ApiConstants {
-
-    public static final String JSON_TYPE = "application/json";
-}
--- UserManager/src/main/java/com/shophub/model/service/ILoginService.java
+++
@@ -1,7 +0,0 @@
-package com.shophub.model.service;
-
-public interface ILoginService<T, E> extends IGenericService<T, E>{
-
-    T findByEmail(final String email);
-    boolean userExist(String email);
-}
